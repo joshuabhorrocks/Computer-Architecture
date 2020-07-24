171e775e5134c005586be1d76b3bc521f173998e
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-07-24 11:47:35 -0600

    Sprint MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,7 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.SP = 7
+        self.fl = [0, 0, 0]
 
 
     def load(self):
@@ -68,29 +69,31 @@ class CPU:
     def ram_write(self, MAR, MDR):
         # MDR (Memory Data Register) = Data that will be read or will be written
         self.ram[MAR] = MDR
-        # print("Updated Ram (self.ram): ", self.ram)
+        return MDR
 
 
-    def alu(self, op, reg_a, reg_b):
+    def alu(self, IR, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
+        if IR == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
 
-        elif op == "MUL":
+        elif IR == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
 
-        elif op == "SUB":
-            if reg_a >= reg_b:
-                self.reg[reg_a] -= self.reg[reg_b]
-            else:
-                self.reg[reg_b] -= self.reg[reg_a]
-
-        elif op == "DIV":
-            if reg_a >= reg_b:
-                self.reg[reg_a] //= self.reg[reg_b]
-            else:
-                self.reg[reg_b] //= self.reg[reg_a]
+        elif IR == "CMP":
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl[0] = 1
+                self.fl[1] = 0
+                self.fl[2] = 0
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl[0] = 0
+                self.fl[1] = 1
+                self.fl[2] = 0
+            elif self.reg[reg_a] == self.reg[reg_b]:
+                self.fl[0] = 0
+                self.fl[1] = 0
+                self.fl[2] = 1
 
         else:
             raise Exception("Unsupported ALU operation")
@@ -104,7 +107,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
+            self.fl,
             #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -116,10 +119,13 @@ class CPU:
 
         print()
 
+    def jump(self):
+        self.pc = self.reg[self.ram_read(self.pc+1)]
 
     def run(self):
         """Run the CPU."""
-        running = True
+        running = True 
+
         HLT = 0b00000001
         LDI = 0b10000010
         PRN = 0b01000111
@@ -129,6 +135,10 @@ class CPU:
         CALL = 0b01010000
         RET = 0b00010001
         ADD = 0b10100000
+        CMP = 0b10100111
+        JMP = 0b01010100
+        JEQ = 0b01010101
+        JNE = 0b01010110
 
         while running:
             # IR (Instruction Register) = info from memory address stored at pc
@@ -169,31 +179,42 @@ class CPU:
                 self.pc += 2
 
             elif IR == CALL:
-                # Get address of the next instruction
                 return_addr = self.pc + 2
-
-                # Push that on the stack
                 self.reg[self.SP] -= 1
                 address_to_push_to = self.reg[self.SP]
                 self.ram[address_to_push_to] = return_addr
-
-                # Set the PC to the subroutine address
                 reg_num = self.ram[self.pc + 1]
                 subroutine_addr = self.reg[reg_num]
-
                 self.pc = subroutine_addr
+
             elif IR == RET:
-                # Get return address from the top of the stack
                 address_to_pop_from = self.reg[self.SP]
                 return_addr = self.ram[address_to_pop_from]
                 self.reg[self.SP] += 1
-
-                # Set the PC to the return address
                 self.pc = return_addr     
 
             elif IR == ADD:
                 self.alu("ADD", reg_a, reg_b)
                 self.pc += 3
+
+            elif IR == CMP:
+                self.alu('CMP', reg_a, reg_b)
+                self.pc += 3
+
+            elif IR == JEQ:
+                if self.fl[2] == 1:
+                    self.jump()
+                else:
+                    self.pc += 2
+
+            elif IR == JNE:
+                if self.fl[2] == 0:
+                    self.jump()
+                else:
+                    self.pc += 2
+
+            elif IR == JMP:
+                self.jump()
 
             else:
                 print(f"Instruction is invalid: {IR} at index {self.pc}")
